4e0f5190804b104c57864fce8f74cfb1bac73130
Mike J. Chen <mjchen@google.com>
2014-08-05 20:27:40 -0700

    Add more overlays and some dpi and heap configs
    Copied from ADT-1.  Makes dpi appropriate for TV and increases
    default dalvik heap size.
    
    Change-Id: I7b80b9300cae0fd6bf6ffba4186521e955335617
    Signed-off-by: Mike J. Chen <mjchen@google.com>

--- device.mk
+++ device.mk
@@ -45,8 +45,15 @@ endif #TARGET_KERNEL_BUILT_FROM_SOURCE
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml
 
-PRODUCT_AAPT_CONFIG := xlarge hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# xhdpi, while we are hardcoding the 1080 resolution.
+# when we start doing 720 as well, will need to stop hardcoding this.
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=320
+
+$(call inherit-product-if-exists, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
