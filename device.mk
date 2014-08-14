52a47284b074233aa1176fadf1ff451ab3b93be4
Patrick Tjin <pattjin@google.com>
2014-08-14 16:17:56 -0700

    fugu: add ethernet support
    Bug: 16471842
    
    Change-Id: I065e8f6fcef81e52e3967f782100941b222dd47a
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- device.mk
+++ device.mk
@@ -205,7 +205,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
 
 # Key layout files
 PRODUCT_COPY_FILES += \
