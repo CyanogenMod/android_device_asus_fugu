0330ef1f29bc858ca1bd19541d73d8c9ac8d54e2
Jerome Poichet <jpoichet@google.com>
2014-09-10 13:30:21 -0700

    Adding keyboard layout matching new remote name
    b/17454715 | [BT pairing] Cannot pair with Nexus Remote (firmware version 057p23/14615)
    
    Change-Id: I69e2444f228b54660c59dbba4ce16e9b4e6bf9ae

--- device.mk
+++ device.mk
@@ -225,7 +225,8 @@ PRODUCT_COPY_FILES += \
     device/asus/fugu/gpio-keys.idc:system/usr/idc/gpio-keys.idc \
     device/asus/fugu/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     device/asus/fugu/gpio-keys.kcm:system/usr/keychars/gpio-keys.kcm \
-    device/asus/fugu/Spike.kl:system/usr/keylayout/Spike.kl
+    device/asus/fugu/Spike.kl:system/usr/keylayout/Spike.kl \
+    device/asus/fugu/Nexus_Remote.kl:system/usr/keylayout/Nexus_Remote.kl
 
 #GFX Config
 PRODUCT_COPY_FILES += \
