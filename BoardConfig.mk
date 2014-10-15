52e48873058190ca5aca6c0d592c3cfa286443bd
Tim Murray <timmurray@google.com>
2014-10-15 14:06:55 -0700

    set BUILD_ARM_FOR_X86 to fix Houdini RS issues
    bug 17765171
    
    Change-Id: I840e00333ccb1e6da10a27535bca780f32d24bc8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,8 @@ COMMON_GLOBAL_CFLAGS += -DGFX_BUF_EXT
 
 # enabled to use IMG RS GPU driver
 OVERRIDE_RS_DRIVER := libPVRRS.so
+# enable ARM codegen for x86 with Houdini
+BUILD_ARM_FOR_X86 := true
 
 # enabled to carry out all drawing operations performed on a View's canvas with GPU for 2D rendering pipeline.
 USE_OPENGL_RENDERER := true
