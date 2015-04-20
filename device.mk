c2e7d210147f427acd99c9d6c73840a07899de6f
dhacker29 <dhackerdvm@gmail.com>
2015-04-19 20:53:51 -0400

    Change back to xhdpi, as long as density is 240 you will get tablet
    layout and apps in market
    
    Change-Id: I337bed933f6d7283fcac95a3e2da5b8563bbd2c6

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ PRODUCT_COPY_FILES += \
     device/asus/fugu/fugu_core_hardware.xml:system/etc/permissions/fugu_core_hardware.xml
 
 PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # xhdpi, while we are hardcoding the 1080 resolution.
 # when we start doing 720 as well, will need to stop hardcoding this.
