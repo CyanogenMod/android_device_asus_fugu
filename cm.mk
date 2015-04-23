a8cbbdc1958ca04ac035db10d5a44ad3960a69d7
dhacker29 <dhackerdvm@gmail.com>
2015-04-22 23:06:42 -0400

    Update fingerprint to 5.1.1
    Change-Id: I6253290fa559324cbcd9af89c6e3aab7e3983f60

--- cm.mk
+++ cm.mk
@@ -18,5 +18,5 @@ PRODUCT_MANUFACTURER := Asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=fugu \
-    BUILD_FINGERPRINT=google/fugu/fugu:5.1/LMY47D/1743759:user/release-keys \
-    PRIVATE_BUILD_DESC="fugu-user 5.1 LMY47D 1743759 release-keys"
+    BUILD_FINGERPRINT=google/fugu/fugu:5.1.1/LMY47V/1836172:user/release-keys \
+    PRIVATE_BUILD_DESC="fugu-user 5.1.1 LMY47V 1836172 release-keys"
