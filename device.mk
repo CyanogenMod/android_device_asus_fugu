5369c31b427b052e0a4f005f36392c9f120b1c92
Steve Kondik <steve@cyngn.com>
2015-06-20 15:23:45 -0400

    fugu: Don't copy FFMPEG config anymore
    Change-Id: I4d038e0d0c5b488bd8b910367f64e75729bb8253

--- device.mk
+++ device.mk
@@ -136,7 +136,6 @@ PRODUCT_COPY_FILES += \
     device/asus/fugu/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     device/asus/fugu/media_codecs.xml:system/etc/media_codecs.xml \
     device/asus/fugu/vp9_interpredict.binary:system/etc/vp9_interpredict.binary \
     device/asus/fugu/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf \
