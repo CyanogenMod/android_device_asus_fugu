1f2e139bbd5f250d85a39866abbf0fc9c861e42e
dhacker29 <dhackerdvm@gmail.com>
2015-11-06 02:02:03 -0500

    fugu: Update fingerprint to MRA58N
    Change-Id: Idb35c8b02009dab41072a8e038f83ff038943ed0

--- cm.mk
+++ cm.mk
@@ -16,5 +16,5 @@ PRODUCT_MANUFACTURER := Asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=fugu \
-    BUILD_FINGERPRINT=google/fugu/fugu:6.0/MRA58K/2256973:user/release-keys \
-    PRIVATE_BUILD_DESC="fugu-user 6.0 MRA58K 2256973 release-keys"
+    BUILD_FINGERPRINT=google/fugu/fugu:6.0/MRA58N/2289998:user/release-keys \
+    PRIVATE_BUILD_DESC="fugu-user 6.0 MRA58N 2289998 release-keys"
