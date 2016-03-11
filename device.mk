a2ee00e332fba187dba91ac43b758363aa55903e
Donghyun Cho <donghyun@google.com>
2016-03-11 17:18:09 +0900

    Disable 'Set Menu Language' feature in fugu
    Bug: 25704899
    Change-Id: Ic46302a3e4af04c526832d260673028e3decb7ac

--- device.mk
+++ device.mk
@@ -95,6 +95,9 @@ PRODUCT_COPY_FILES += \
 # Hdmi CEC: Fugu works as a playback device (4).
 PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4
 
+# Hdmi CEC: Disable 'Set Menu Language' feature.
+PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.set_menu_language=false
+
 # Boot Animation
 PRODUCT_COPY_FILES += \
     device/asus/fugu/bootanimation-580-256col.zip:system/media/bootanimation.zip
