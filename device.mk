003c96d909d4315a7a6784e0c5e257a45b6d96ad
Jesse Hall <jessehall@google.com>
2016-03-15 09:23:42 -0700

    Add Vulkan system features
    Change-Id: I7001e63bb7b49c1fb542d95a8e1093599ea93bc9

--- device.mk
+++ device.mk
@@ -239,7 +239,9 @@ PRODUCT_COPY_FILES += \
 
 #GFX Config
 PRODUCT_COPY_FILES += \
-    device/asus/fugu/powervr.ini:system/etc/powervr.ini
+    device/asus/fugu/powervr.ini:system/etc/powervr.ini \
+    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
 # Thermal itux
 ENABLE_ITUXD := true
